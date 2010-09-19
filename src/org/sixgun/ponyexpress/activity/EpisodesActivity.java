scifly1/PonyExpress

commit 32a9845f77dd8240e740a20515d4cd87848f4428

Added support for other Sixgun productions podcasts (Rant-o'Fab).
Added a new Podcasts table to the database.  This holds the name of the podcast, the feed url, the albumn art url and the name of the table in which to store the episodes.
Each podcasts episodes are stored in a unique table and the ogg files are now stored in a different folder for each podcast.
EpisodeActivity is now generic and works for any podcast.
All of the methods that act on Episode tables have been modified to also take the name of the podcast as a parameter, in order to act on teh correct table.

res/values/strings.xml
@@ -31,5 +31,8 @@
 	<string name="account_setup">Enter your Identi.ca account details.</string>
 	<string name="pause">Pause</string>
 	<string name="play">Play</string>
-<string-array name="Podcasts"><item>Linux Outlaws</item><item>Rant-o\'Fab</item></string-array>
+<!--	The sixgun Podcasts array holds the default podcasts that are added when first installed-->
+<string-array name="Sixgun_Podcasts"><item>http://feeds.feedburner.com/linuxoutlaws-ogg</item><item>http://feeds.feedburner.com/rantofab-ogg</item></string-array>
+
+
 </resources>

src/org/sixgun/ponyexpress/activity/EpisodesActivity.java
@@ -22,6 +22,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.sixgun.ponyexpress.EpisodeKeys;
+import org.sixgun.ponyexpress.PodcastKeys;
 import org.sixgun.ponyexpress.PonyExpressApp;
 import org.sixgun.ponyexpress.R;
 
@@ -39,7 +40,8 @@ import android.widget.TextView;
 public class EpisodesActivity extends ListActivity {
 
 	private static final String TAG = "EpisodesActivity";
-	private PonyExpressApp mPonyExpressApp; 
+	private PonyExpressApp mPonyExpressApp;
+	private String mPodcastName; 
 
 	
 	@Override
@@ -47,10 +49,9 @@ public class EpisodesActivity extends ListActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.episodes);
 		TextView title = (TextView)findViewById(R.id.title);
-		//FIXME This string should not be hard coded, 
-		//it should come from the database.
-		//We can get it from the feed at <channel><title>...</></>
-		title.setText("Linux Outlaws");
+		//Get Podcast name from bundle and set title text.
+		mPodcastName = getIntent().getExtras().getString(PodcastKeys.NAME);
+		title.setText(mPodcastName);
 		
 		//Get the application context.
 		mPonyExpressApp = (PonyExpressApp)getApplication();
@@ -73,8 +74,8 @@ public class EpisodesActivity extends ListActivity {
 	protected void onListItemClick(ListView l, View v, int position, long id) {
 		super.onListItemClick(l, v, position, id);
 		//Get all info from database and put it in an Intent for EpisodeTabs
-		final String title = mPonyExpressApp.getDbHelper().getEpisodeTitle(id);
-		final String description = mPonyExpressApp.getDbHelper().getDescription(id);
+		final String title = mPonyExpressApp.getDbHelper().getEpisodeTitle(id, mPodcastName);
+		final String description = mPonyExpressApp.getDbHelper().getDescription(id, mPodcastName);
 		//Seperate episode number from filename for hashtag.
 		Pattern digits = Pattern.compile("[0-9]+");
 		Matcher m = digits.matcher(title);
@@ -83,21 +84,22 @@ public class EpisodesActivity extends ListActivity {
 		Log.d(TAG, "Episode number: " + epNumber);
 
 		Intent intent = new Intent(this,EpisodeTabs.class);
+		intent.putExtra(PodcastKeys.NAME, mPodcastName);
 		intent.putExtra(EpisodeKeys.TITLE, title);
 		intent.putExtra(EpisodeKeys.DESCRIPTION, description);
 		intent.putExtra(EpisodeKeys.EP_NUMBER, epNumber);
 		intent.putExtra(EpisodeKeys._ID, id);
 		//Determine if Episode has been downloaded and add required extras.
-		final boolean downloaded = mPonyExpressApp.getDbHelper().isEpisodeDownloaded(id);
+		final boolean downloaded = mPonyExpressApp.getDbHelper().isEpisodeDownloaded(id, mPodcastName);
 		if (downloaded){
-			final String filename = mPonyExpressApp.getDbHelper().getEpisodeFilename(id);
+			final String filename = mPonyExpressApp.getDbHelper().getEpisodeFilename(id, mPodcastName);
 			intent.putExtra(EpisodeKeys.FILENAME, filename);
-			final int listened = mPonyExpressApp.getDbHelper().getListened(id);
+			final int listened = mPonyExpressApp.getDbHelper().getListened(id, mPodcastName);
 			intent.putExtra(EpisodeKeys.LISTENED, listened);
 		} else {
-			final String url = mPonyExpressApp.getDbHelper().getEpisodeUrl(id);
+			final String url = mPonyExpressApp.getDbHelper().getEpisodeUrl(id, mPodcastName);
 			intent.putExtra(EpisodeKeys.URL, url);
-			final int size = mPonyExpressApp.getDbHelper().getEpisodeSize(id);
+			final int size = mPonyExpressApp.getDbHelper().getEpisodeSize(id, mPodcastName);
 			intent.putExtra(EpisodeKeys.SIZE, size);
 		}
 		startActivity(intent);
@@ -109,7 +111,7 @@ public class EpisodesActivity extends ListActivity {
 	 * Query the database for all Episode titles to populate the ListView.
 	 */
 	private void listEpisodes(){
-		Cursor c = mPonyExpressApp.getDbHelper().getAllEpisodeNames();
+		Cursor c = mPonyExpressApp.getDbHelper().getAllEpisodeNames(mPodcastName);
 		startManagingCursor(c);
 		//Set up columns to map from, and layout to map to
 		String[] from = new String[] { EpisodeKeys.TITLE };

src/org/sixgun/ponyexpress/activity/PonyExpressActivity.java
@@ -19,6 +19,7 @@
 package org.sixgun.ponyexpress.activity;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.Iterator;
@@ -28,6 +29,7 @@ import java.util.Map.Entry;
 
 import org.sixgun.ponyexpress.Episode;
 import org.sixgun.ponyexpress.EpisodeKeys;
+import org.sixgun.ponyexpress.PodcastKeys;
 import org.sixgun.ponyexpress.PonyExpressApp;
 import org.sixgun.ponyexpress.R;
 import org.sixgun.ponyexpress.util.EpisodeFeedParser;
@@ -35,6 +37,7 @@ import org.sixgun.ponyexpress.util.EpisodeFeedParser;
 import android.app.ListActivity;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
@@ -42,10 +45,10 @@ import android.os.AsyncTask.Status;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
 
 /**
@@ -181,15 +184,25 @@ public class PonyExpressActivity extends ListActivity {
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
 		super.onListItemClick(l, v, position, id);
-		//TODO We only have LO so this always starts LO episodes at present.
+		//Get the podcast name.
+		final String name = mPonyExpressApp.getDbHelper().getPodcastName(id);
+		//Store in an intent and send to EpisodesActivity
 		Intent intent = new Intent(this,EpisodesActivity.class);
+		intent.putExtra(PodcastKeys.NAME, name);
 		startActivity(intent);
 		
 	}
 	
 	private void listPodcasts() {
-		String[] podcasts = getResources().getStringArray(R.array.Podcasts);
-		setListAdapter(new ArrayAdapter<String>(this, R.layout.podcast_row, podcasts));
+		Cursor c = mPonyExpressApp.getDbHelper().getAllPodcastNames();
+		startManagingCursor(c);
+		//Set up columns to map from, and layout to map to
+		String[] from = new String[] { PodcastKeys.NAME };
+		int[] to = new int[] { R.id.podcast_text };
+		
+		SimpleCursorAdapter podcasts = new SimpleCursorAdapter(
+				this, R.layout.podcast_row, c, from, to);
+		setListAdapter(podcasts);
 	}
 
 	/**
@@ -217,37 +230,44 @@ public class PonyExpressActivity extends ListActivity {
 		 */
 		@Override
 		protected Void doInBackground(Void... params) {
+			//Get each podcast name then its feed url, and update each.
+			List<String> podcast_names = 
+				mPonyExpressApp.getDbHelper().listAllPodcasts();
 			
-			final String feed = "http://feeds.feedburner.com/linuxoutlaws-ogg";
-
-			EpisodeFeedParser parser = new EpisodeFeedParser(mPonyExpressApp,feed);
-
-			List<Episode> episodes = parser.parse();
-
-			final Date date = mPonyExpressApp.getDbHelper().getLatestEpisodeDate();
-						
-			for (Episode episode: episodes){
-				//Add any new episodes
-				if (episode.getDate().compareTo(date) > 0) {
-					mPonyExpressApp.getDbHelper().insertEpisode(episode);
-				}		
+			for (String podcast: podcast_names){
+				String podcast_url = 
+					mPonyExpressApp.getDbHelper().getPodcastUrl(podcast);
+				EpisodeFeedParser parser = new EpisodeFeedParser(mPonyExpressApp,
+						podcast_url);
+				List<Episode> episodes = parser.parse();
 				
-				//Determine how many episodes to remove
-				final int rows = mPonyExpressApp.getDbHelper().getNumberOfRows();
-				final int episodesToDelete = rows - mEpisodesToHold;
-				//Remove correct number of episodes from oldest episodes to maintain required number.
-				for (int i = episodesToDelete; i > 0; i--){
-					final long rowID = 
-						mPonyExpressApp.getDbHelper().getOldestEpisode();
-					if (rowID != -1){
-						if (mPonyExpressApp.getDbHelper().isEpisodeDownloaded(rowID)){
-							//delete from SD Card
-							deleteFile(rowID);
-						}
-						//remove from database after deleting.
-						mPonyExpressApp.getDbHelper().deleteEpisode(rowID);
-					} else {Log.e(TAG, "Cannot find oldest episode");}
+				final Date date = 
+					mPonyExpressApp.getDbHelper().getLatestEpisodeDate(podcast);
+				
+				for (Episode episode: episodes){
+					//Add any new episodes
+					if (episode.getDate().compareTo(date) > 0) {
+						mPonyExpressApp.getDbHelper().insertEpisode(episode, podcast);
+					}		
+					
+					//Determine how many episodes to remove
+					final int rows = mPonyExpressApp.getDbHelper().getNumberOfRows(podcast);
+					final int episodesToDelete = rows - mEpisodesToHold;
+					//Remove correct number of episodes from oldest episodes to maintain required number.
+					for (int i = episodesToDelete; i > 0; i--){
+						final long rowID = 
+							mPonyExpressApp.getDbHelper().getOldestEpisode(podcast);
+						if (rowID != -1){
+							if (mPonyExpressApp.getDbHelper().isEpisodeDownloaded(rowID, podcast)){
+								//delete from SD Card
+								deleteFile(rowID, podcast);
+							}
+							//remove from database after deleting.
+							mPonyExpressApp.getDbHelper().deleteEpisode(rowID, podcast);
+						} else {Log.e(TAG, "Cannot find oldest episode");}
+					}
 				}
+				
 			}
 			return null;
 		}
@@ -255,10 +275,12 @@ public class PonyExpressActivity extends ListActivity {
 		 * 
 		 * @param rowID of the file to be deleted from the database.
 		 */
-		private void deleteFile(long rowID) {
+		private void deleteFile(long rowID, String podcast_name) {
 			File rootPath = Environment.getExternalStorageDirectory();
 			File dirPath = new File(rootPath,PonyExpressApp.PODCAST_PATH);
-			String filename = mPonyExpressApp.getDbHelper().getEpisodeFilename(rowID);
+			String filename = mPonyExpressApp.getDbHelper().getEpisodeFilename(rowID, podcast_name);
+			//Add the podcast name as a folder under the PODCAST_PATH
+			filename = podcast_name + filename;
 			File fullPath = new File(dirPath,filename);
 			fullPath.delete();			
 		}
@@ -292,23 +314,29 @@ public class PonyExpressActivity extends ListActivity {
 
 		@Override
 		protected Void doInBackground(Void... params) {
-			//Get a List of the file names that are on the SD Card.
+			//Get a List of the file names that are on the SD Card for each podcast.
 			File rootPath = Environment.getExternalStorageDirectory();
 			File path = new File(rootPath,PonyExpressApp.PODCAST_PATH);
-			final String[] filesOnDisk = path.list();
-			//Get a Map of filenames (and their index) that are in the database
-			final Map<Long, String> filesInDatabase = 
-				mPonyExpressApp.getDbHelper().getFilenamesOnDisk();
-			if (filesOnDisk != null){
-				List<String> diskFiles = Arrays.asList(filesOnDisk);
-				//If file is in database but not on disk, mark as not downloaded in database.
-				final int mapSize = filesInDatabase.size();
-				Iterator<Entry<Long, String>> fileIter = filesInDatabase.entrySet().iterator();
-				for (int i = 0; i < mapSize; i++){
-					Map.Entry<Long, String> entry = (Map.Entry<Long, String>)fileIter.next();
-					if (!diskFiles.contains(entry.getValue())){
-						mPonyExpressApp.getDbHelper().update(entry.getKey(), 
-								EpisodeKeys.DOWNLOADED, "false");
+			final String[] podcastsOnDisk = path.list();
+			List<String> filesOnDisk = new ArrayList<String>();
+			if (podcastsOnDisk != null){
+				for (String podcast: podcastsOnDisk){
+					String[] files = path.list();
+					filesOnDisk.addAll(Arrays.asList(files));
+					//Get a Map of filenames (and their index) that are in the database
+					final Map<Long, String> filesInDatabase = 
+						mPonyExpressApp.getDbHelper().getFilenamesOnDisk(podcast);
+					if (filesOnDisk != null){
+						//If file is in database but not on disk, mark as not downloaded in database.
+						final int mapSize = filesInDatabase.size();
+						Iterator<Entry<Long, String>> fileIter = filesInDatabase.entrySet().iterator();
+						for (int i = 0; i < mapSize; i++){
+							Map.Entry<Long, String> entry = (Map.Entry<Long, String>)fileIter.next();
+							if (!filesOnDisk.contains(entry.getValue())){
+								mPonyExpressApp.getDbHelper().update(podcast, entry.getKey(), 
+										EpisodeKeys.DOWNLOADED, "false");
+							}
+						}
 					}
 				}
 			}

commit 2dc6b3305bac99013e1147f1ece09615f988b9bb

UI improvments and a bugfix for a regression introduced in commit 32a9845f.
Album art is now downloaded and displayed with the podcast listing and on the episodes activity.
The title of the Episodes Activity is now the title of the podcast whose episodes are being listed.
Added the sixgun productions logo to the podcasts listing.
BugFix: the database consistency check could not find any downloaded episodes and so was marking then not downloaded in the database.

AndroidManifest.xml
@@ -5,8 +5,7 @@
 	<application android:icon="@drawable/pony_icon"
 		android:label="@string/app_name" android:debuggable="true"
 		android:name="PonyExpressApp">
-		<activity android:name=".activity.PonyExpressActivity"
-			android:label="@string/app_name">
+		<activity android:name=".activity.PonyExpressActivity">
 			<intent-filter>
 				<action android:name="android.intent.action.MAIN" />
 				<category android:name="android.intent.category.LAUNCHER" />
@@ -19,7 +18,7 @@
 		<activity android:name=".activity.EpisodesActivity" />
 		<activity android:name=".activity.PlayerActivity" />
 		<activity android:name=".activity.DownloadActivity" />
-		<activity android:name=".activity.EpisodeTabs" android:theme="@android:style/Theme.NoTitleBar" 
+		<activity android:name=".activity.EpisodeTabs"  
 		android:windowSoftInputMode="adjustResize"/>
 		<activity android:name=".activity.EpisodeNotesActivity" />
 		<activity android:name=".activity.IdenticaActivity" 

res/layout/episode_tabs.xml
@@ -6,7 +6,7 @@
 		android:layout_width="fill_parent" android:layout_height="fill_parent"
 		android:padding="5dp">
 		<TextView android:id="@+id/TitleText" android:layout_width="wrap_content"
-			android:layout_height="wrap_content" android:textSize="18sp"
+			android:layout_height="wrap_content" android:textSize="18sp" android:textStyle="bold"
 			android:layout_centerHorizontal="true" />
 		<TabWidget android:id="@android:id/tabs"
 			android:layout_width="fill_parent" android:layout_height="wrap_content" />

res/layout/episodes.xml
@@ -2,12 +2,19 @@
 <RelativeLayout xmlns:android="http://schemas.android.com/apk/res/android"
 	android:orientation="vertical" android:layout_width="fill_parent"
 	android:layout_height="fill_parent">
+	<org.sixgun.ponyexpress.view.RemoteImageView
+		android:id="@+id/album_art" android:layout_width="wrap_content"
+		android:layout_height="wrap_content" android:layout_alignParentLeft="true"
+		android:adjustViewBounds="true" android:padding="3dp"
+		android:maxWidth="70dp" android:maxHeight="70dp"
+		android:scaleType="fitCenter" />
+	
 	<TextView android:id="@+id/title" android:layout_width="wrap_content"
 		android:layout_height="wrap_content" android:text="@string/Episodes_title"
-		android:padding="10dp" android:textSize="30sp" />
+		 android:textSize="25sp" android:textStyle="bold"
+		android:layout_toRightOf="@+id/album_art" />
 	
-
 	<ListView android:id="@android:id/list" android:layout_width="wrap_content"
-		android:layout_height="wrap_content" android:layout_below="@id/title"
+		android:layout_height="wrap_content" android:layout_below="@id/album_art"
 		android:paddingTop="5dp" />
 </RelativeLayout>

res/layout/main.xml
@@ -2,9 +2,20 @@
 <RelativeLayout xmlns:android="http://schemas.android.com/apk/res/android"
 	android:orientation="vertical" android:layout_width="fill_parent"
 	android:layout_height="fill_parent">
+	<ImageView android:id="@+id/sixgunlogo"
+     	android:layout_width="wrap_content"
+     	android:layout_height="wrap_content"
+     	android:layout_alignParentLeft="true"
+     	android:layout_alignParentTop="true"
+     	android:src="@drawable/sixgunlogo"
+     	android:paddingRight="3dp"
+     	android:maxWidth="55dp"
+     	android:maxHeight="55dp"
+     	android:adjustViewBounds="true" />
 	<TextView android:id="@+id/sixguntitle" android:layout_width="wrap_content"
-		android:layout_height="wrap_content" android:text="@string/Sixgun_title"
-		android:padding="10dp" android:textSize="30sp" />
+		android:layout_height="wrap_content" android:text="@string/app_name"
+		android:textStyle="bold" android:padding="10dp" 
+		android:textSize="25sp" android:layout_toRightOf="@+id/sixgunlogo"/>
 
 	<ListView android:id="@android:id/list" android:layout_width="wrap_content"
 		android:layout_height="wrap_content" android:layout_below="@id/sixguntitle"

res/layout/podcast_row.xml
@@ -1,7 +1,14 @@
 <?xml version="1.0" encoding="utf-8"?>
-<TextView android:id="@+id/podcast_text"
-    xmlns:android="http://schemas.android.com/apk/res/android"
-    android:layout_width="wrap_content"
-    android:layout_height="wrap_content"
-    android:padding="10dp"
-    android:textSize="16sp"/>
+<RelativeLayout xmlns:android="http://schemas.android.com/apk/res/android"
+	android:layout_width="fill_parent" android:layout_height="fill_parent">
+	<org.sixgun.ponyexpress.view.RemoteImageView
+		android:id="@+id/album_art" android:layout_width="wrap_content"
+		android:layout_height="wrap_content" android:layout_alignParentLeft="true"
+		android:adjustViewBounds="true" android:paddingRight="3dp"
+		android:paddingTop="1dp" android:paddingBottom="2dp" android:maxWidth="70dp" android:maxHeight="70dp"
+		android:scaleType="fitCenter" />
+
+	<TextView android:id="@+id/podcast_text" android:layout_width="wrap_content"
+		android:layout_height="wrap_content" android:layout_toRightOf="@+id/album_art"
+		android:padding="10dp" android:textSize="16sp" />
+</RelativeLayout>

res/values/strings.xml
@@ -5,9 +5,7 @@
 	<string name="Episodes_title">
 		<b>Episodes</b>
 	</string>
-	<string name="Sixgun_title">
-		<b>Sixgun Productions</b>
-	</string>
+	<string name="Sixgun_title">Sixgun Productions</string>
 	<string name="no_internet_connection">Cannot connect to the Internet. Check your
 		connectivity.</string>
 	<string name="download">Download</string>
@@ -35,5 +33,5 @@
 <string-array name="Sixgun_Podcasts"><item>http://feeds.feedburner.com/linuxoutlaws-ogg</item><item>http://feeds.feedburner.com/rantofab-ogg</item></string-array>
 <!-- The Sixgun podcasts tags array holds the identi.ca tags for each podcast.  They need to be in the same order as the podcasts
 Tags for other podcasts will be added by the user when they add the feed url -->
-<string-array name="Sixgun_Podcast_Tags"><item>lo</item><item>"rantofab"</item></string-array>
+<string-array name="Sixgun_Podcast_Tags"><item>lo</item><item>rantofab</item></string-array>
 </resources>

src/org/sixgun/ponyexpress/activity/EpisodesActivity.java
@@ -25,6 +25,7 @@ import org.sixgun.ponyexpress.EpisodeKeys;
 import org.sixgun.ponyexpress.PodcastKeys;
 import org.sixgun.ponyexpress.PonyExpressApp;
 import org.sixgun.ponyexpress.R;
+import org.sixgun.ponyexpress.view.RemoteImageView;
 
 import android.app.ListActivity;
 import android.content.Intent;
@@ -52,6 +53,13 @@ public class EpisodesActivity extends ListActivity {
 		//Get Podcast name from bundle and set title text.
 		mPodcastName = getIntent().getExtras().getString(PodcastKeys.NAME);
 		title.setText(mPodcastName);
+		//Get Album art url and set image.
+		RemoteImageView albumArt = (RemoteImageView)findViewById(R.id.album_art);
+		String albumArtUrl = getIntent().getExtras().getString(PodcastKeys.ALBUM_ART_URL);
+		if (albumArtUrl!= null && !"".equals(albumArtUrl) && !"null".equalsIgnoreCase(albumArtUrl)){
+    		albumArt.setRemoteURI(albumArtUrl);
+    		albumArt.loadImage();
+		}
 		
 		//Get the application context.
 		mPonyExpressApp = (PonyExpressApp)getApplication();

src/org/sixgun/ponyexpress/activity/IdenticaActivity.java
@@ -259,7 +259,7 @@ public class IdenticaActivity extends ListActivity {
 	/**
 	 * We subclass ArrayAdapter to handle our specific Dent ListArray.  The override of
 	 * getView() provides a mapping between the fields of a Dent we want to view and the 
-	 * TextView instances in which we want them to appear.
+	 * TextView/ImageView instances in which we want them to appear.
 	 *
 	 */
 	private class DentAdapter extends ArrayAdapter<Dent> {

src/org/sixgun/ponyexpress/activity/PonyExpressActivity.java
@@ -33,9 +33,11 @@ import org.sixgun.ponyexpress.PodcastKeys;
 import org.sixgun.ponyexpress.PonyExpressApp;
 import org.sixgun.ponyexpress.R;
 import org.sixgun.ponyexpress.util.EpisodeFeedParser;
+import org.sixgun.ponyexpress.view.RemoteImageView;
 
 import android.app.ListActivity;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.AsyncTask;
@@ -43,12 +45,15 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.os.AsyncTask.Status;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.View.OnClickListener;
+import android.widget.CursorAdapter;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 /**
@@ -66,10 +71,12 @@ public class PonyExpressActivity extends ListActivity {
 	private ProgressDialog mProgDialog;
 	private ProgressDialog mProgDialogDb;
 	private int mEpisodesToHold = 10;
+	private Cursor mPodcastCursor;
 	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		setTitle(R.string.Sixgun_title);
 		
 		//Get the application context.
 		mPonyExpressApp = (PonyExpressApp)getApplication();
@@ -184,27 +191,63 @@ public class PonyExpressActivity extends ListActivity {
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
 		super.onListItemClick(l, v, position, id);
-		//Get the podcast name.
+		//Get the podcast name and album art url.
 		final String name = mPonyExpressApp.getDbHelper().getPodcastName(id);
+		final String url = mPonyExpressApp.getDbHelper().getAlbumArtUrl(id);
 		//Store in an intent and send to EpisodesActivity
 		Intent intent = new Intent(this,EpisodesActivity.class);
 		intent.putExtra(PodcastKeys.NAME, name);
+		intent.putExtra(PodcastKeys.ALBUM_ART_URL, url);
 		startActivity(intent);
 		
 	}
 	
 	private void listPodcasts() {
-		Cursor c = mPonyExpressApp.getDbHelper().getAllPodcastNames();
-		startManagingCursor(c);
-		//Set up columns to map from, and layout to map to
-		String[] from = new String[] { PodcastKeys.NAME };
-		int[] to = new int[] { R.id.podcast_text };
-		
-		SimpleCursorAdapter podcasts = new SimpleCursorAdapter(
-				this, R.layout.podcast_row, c, from, to);
-		setListAdapter(podcasts);
+		mPodcastCursor = mPonyExpressApp.getDbHelper().getAllPodcastNamesAndArt();
+		startManagingCursor(mPodcastCursor);
+		//Create a CursorAdapter to map podcast title and art to the ListView.
+		PodcastCursorAdapter adapter = new PodcastCursorAdapter(mPonyExpressApp, mPodcastCursor);
+		setListAdapter(adapter);
 	}
 
+	/**
+	 * We subclass CursorAdapter to handle our display the results from our Podcast cursor. 
+	 * Overide newView to create/inflate a view to bind the data to.
+	 * Overide bindView to determine how the data is bound to the view.
+	 */
+	private class PodcastCursorAdapter extends CursorAdapter{
+
+		public PodcastCursorAdapter(Context context, Cursor c) {
+			super(context, c);
+		}
+
+		@Override
+		public void bindView(View view, Context context, Cursor cursor) {
+			final int nameIndex = cursor.getColumnIndex(PodcastKeys.NAME);
+			final int artUrlIndex = cursor.getColumnIndex(PodcastKeys.ALBUM_ART_URL);
+			
+			TextView podcastName = (TextView) view.findViewById(R.id.podcast_text);
+			RemoteImageView albumArt = (RemoteImageView)view.findViewById(R.id.album_art);
+			String name = cursor.getString(nameIndex);
+			podcastName.setText(name);
+			String albumArtUrl = cursor.getString(artUrlIndex);
+			if (albumArtUrl!= null && !"".equals(albumArtUrl) && !"null".equalsIgnoreCase(albumArtUrl)){
+        		albumArt.setRemoteURI(albumArtUrl);
+        		albumArt.loadImage();
+			}
+			
+		}
+
+		@Override
+		public View newView(Context context, Cursor cursor, ViewGroup parent) {
+			LayoutInflater vi = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			View v = new View(context);
+			v = vi.inflate(R.layout.podcast_row, null);
+			return v;
+		}
+		
+	}
+	
 	/**
 	 * Parse the RSS feed and update the database with new episodes in a background thread.
 	 * 
@@ -321,7 +364,8 @@ public class PonyExpressActivity extends ListActivity {
 			List<String> filesOnDisk = new ArrayList<String>();
 			if (podcastsOnDisk != null){
 				for (String podcast: podcastsOnDisk){
-					String[] files = path.list();
+					File podcast_path = new File(path,podcast);
+					String[] files = podcast_path.list();
 					filesOnDisk.addAll(Arrays.asList(files));
 					//Get a Map of filenames (and their index) that are in the database
 					final Map<Long, String> filesInDatabase = 

src/org/sixgun/ponyexpress/util/PonyExpressDbAdaptor.java
@@ -521,8 +521,8 @@ public class PonyExpressDbAdaptor {
 	 * Gets all unique Podcast names from the database.
 	 * @return A Cursor object, which is positioned before the first entry
 	 */
-	public Cursor getAllPodcastNames() {
-		final String[] columns = {PodcastKeys._ID, PodcastKeys.NAME};
+	public Cursor getAllPodcastNamesAndArt() {
+		final String[] columns = {PodcastKeys._ID, PodcastKeys.NAME, PodcastKeys.ALBUM_ART_URL};
 		return mDb.query(
 				true,PODCAST_TABLE,columns,null,null,null,null,PodcastKeys.NAME ,null);
 	}
@@ -547,6 +547,19 @@ public class PonyExpressDbAdaptor {
 		final Cursor cursor = mDb.query(true, PODCAST_TABLE,
 				columns, PodcastKeys.NAME + "=" + quotedName ,
 				null, null, null, null, null);
+		String url = "";
+		if (cursor != null){
+			cursor.moveToFirst();
+			url = cursor.getString(1);
+		}
+		cursor.close();
+		return url;
+	}
+	
+	public String getAlbumArtUrl(long row_ID){
+		final String[] columns = {PodcastKeys._ID,PodcastKeys.ALBUM_ART_URL};
+		final Cursor cursor = mDb.query(true, PODCAST_TABLE,
+				columns, EpisodeKeys._ID + "=" + row_ID, null, null, null, null, null);
 		String url = "";
 		if (cursor != null){
 			cursor.moveToFirst();