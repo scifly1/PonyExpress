5dc2da9ef4eb71e5cff6bd3ce8ce3a752b848526
jawsdaws <jdelectronics1@gmail.com>
2011-11-30 20:48:54 -0500

    Changed album art namespace to look for the itunes NS. Itunes appears to be the standard.

--- src/org/sixgun/ponyexpress/util/PodcastFeedParser.java
+++ src/org/sixgun/ponyexpress/util/PodcastFeedParser.java
@@ -38,10 +38,10 @@ import android.util.Xml;
  */
 public class PodcastFeedParser extends BaseFeedParser {
 
-	static final String MEDIA_NS = "http://search.yahoo.com/mrss/";
+	static final String MEDIA_NS = "http://www.itunes.com/dtds/podcast-1.0.dtd";
 	// names of the XML tags
     static final String NAME = "title";
-    static final String ALBUM_ART_URL = "thumbnail";
+    static final String ALBUM_ART_URL = "image";
 	private static final String TAG = "Pony/PodcastFeedParser";
    
     
@@ -87,7 +87,7 @@ public class PodcastFeedParser extends BaseFeedParser {
 			
 			@Override
 			public void start(Attributes attributes) {
-				String url = attributes.getValue("", "url");
+				String url = attributes.getValue("", "href");
 				Log.d(TAG,"Podcast art from: " + url);
 				new_podcast.setArt_Url(url);				
 			}
@@ -132,7 +132,7 @@ public class PodcastFeedParser extends BaseFeedParser {
 			
 			@Override
 			public void start(Attributes attributes) {
-				String url = attributes.getValue("", "url");
+				String url = attributes.getValue("", "href");
 				Log.d(TAG,"Podcast art from: " + url);
 				new_podcast.setArt_Url(url);				
 			}
