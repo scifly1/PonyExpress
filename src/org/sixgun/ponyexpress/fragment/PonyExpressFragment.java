c3ec62be3895e6612b62fa331465a3c63d66c72c
Paul Elms <scifly@uprocera.co.uk>
2013-07-18 15:02:03 +0100

    Begun to move to using fragments. This will enable multipane layouts on devices large enough to handle them. So far added a new launch activity (PonyExpressFragsActivity) that has the layouts for handling fragments on regular screen sizes and large landscape screen sizes. The beginnings of putting the main pony activity into PonyExpressFragment.

---
+++ src/org/sixgun/ponyexpress/fragment/PonyExpressFragment.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2013 Paul Elms
+ *
+ *  This file is part of PonyExpress.
+ *
+ *  PonyExpress is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  PonyExpress is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with PonyExpress.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package org.sixgun.ponyexpress.fragment;
+
+
+import org.sixgun.ponyexpress.R;
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class PonyExpressFragment extends ListFragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		
+		return inflater.inflate(R.layout.main, container, false);
+	}
+	
+	
+}
